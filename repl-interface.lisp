967efc594705203379c50cbcd8e9cfd3b73230d6
Izaak Walton <izaakw@protonmail.com>
2022-04-19 00:32:30 -0600

    brainstorming interface

--- repl-interface.lisp
+++ repl-interface.lisp
@@ -9,15 +9,54 @@
 ;;; Would you like to edit an appointment, client, employee, or room?
 ;;; What would you like to edit?
 
-(defun prompt-read (prompt)
-  (format *query-io* "~a: " prompt)
-  (force-output *query-io*)
-  (read-line *query-io*))
+(defvar *interface-commands* '(("edit" #'interface-edit)
+			       ("browse" #'interface-browse)
+                               ("add" #'interface-add)))
 
+(defvar *error-response* "I would prefer not to")
+
+(defun browse-prompt ()
+  (format nil "What would you like to browse?~%appointments~%clients~%employees~%rooms~%?"))
+
+(defun interface-browse (&optional category)
+  "Browses items, if not prompts with categories."
+  (if category
+      (browse category)
+      (browse-prompt)))
+
+(defun main-screen ()
+  (format nil "Welcome to Bartleby the Scheduler.")
+  (format nil "type \"help\" for help"))
+
+(defun help-sheet ()
+  (format nil "Bartleby Help Menu: "))
+
+  ;edit...
+   ;     ...clients
+    ;    ...employees
+   ;    ...appointments
+					;rooms
+;add...
+      ;...client
+      ;...appointment
+;browse ... clients employees appointments etc
+;search.... .....
+;
+
+(defgeneric interface-add (object)
+  (:documentation "Prompt for adding an object through the interface"))
+
+(defgeneric interface-edit (object))
+
+(defun interface-browse (object-list))
+
+(defgeneric 
 ;;;search functions
-
+(defvar *search-results* "The current list of search results, for ease of access."
 ;(defun client-interface ()
   
 
 ;(defun interface ()
  ; (prompt-read 
+
+			  
