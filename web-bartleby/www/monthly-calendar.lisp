dd3441783da850723cbf2086d5bc018b95680958
Izaak Walton <izaakw@protonmail.com>
2022-07-26 14:07:02 -0600

    fixing asd

--- web-bartleby/www/monthly-calendar.lisp
+++ web-bartleby/www/monthly-calendar.lisp
@@ -1,12 +1,20 @@
-;;;; web-bartleby/monthly-calendar.lisp
+;;;; web-bartleby/www/monthly-calendar.lisp
 ;;;;
 ;;;; Copyright Izaak Walton (c) 2022
 
 (in-package :web-bartleby)
 
-(defmethod monthly-calendar-day ((date date))
+(defmethod monthly-calendar-day ((date bartleby::date))
+  (format nil "~a   ~a"
+          (bartleby::d date)
+          (if (bartleby::appointments date)
+              (concatenate 'string
+                           "("
+                           (write-to-string (length (bartleby::appointments date)))
+                           ")")
+              " ")))
    
-(hunchentoot:define-easy-handler (monthly-calendar :uri "/monthly-calendar") (&optional date)
+(hunchentoot:define-easy-handler (monthly-calendar :uri "/monthly-calendar") (&optional select-date)
   (with-page (:title "Monthly Calendar")
     (:h1 "Monthly Calendar")
     (cl-bootstrap:bs-table
@@ -20,7 +28,11 @@
         (:th "Friday")
         (:th "Saturday")))
       (:tbody
-       (spinneret:with-html (loop :for w :in (bartleby::gather-weeks (bartleby::month (if date date (bartleby::today))))
+       (spinneret:with-html
+         (loop :for w :in (bartleby::gather-weeks (bartleby::month
+                                                   (if select-date
+                                                       select-date
+                                                       (bartleby::today))))
                                   :do (:tr (loop :for d :in (bartleby::days w)
-                                                 :do (:td (format nil "~a" (bartleby::d d)))))))))))
+                                                 :do (:td (monthly-calendar-day d))))))))))
 
