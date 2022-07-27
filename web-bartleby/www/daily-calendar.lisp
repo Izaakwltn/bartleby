16832f0fb16cc360627dbc0aa4fc3b21122c4159
Izaak Walton <izaakw@protonmail.com>
2022-07-27 01:13:05 -0600

    working on daily calendar

--- web-bartleby/www/daily-calendar.lisp
+++ web-bartleby/www/daily-calendar.lisp
@@ -4,3 +4,14 @@
 
 (in-package :web-bartleby)
 
+(hunchentoot:define-easy-handler (daily-calendar :uri "/daily-calendar") (&optional date)
+  (let ((select-date (if date date (bartleby::today))))
+    (with-page (:title "Daily Calendar")
+      (:h1 "Daily Calendar")
+      (cl-bootstrap:bs-table
+	(:thead
+	 (:tr
+	  (:th select-date)))
+	(:tbody (spinneret:with-html
+		  (loop :for a :in (bartleby::appointments select-date)
+			:do (:tr (:td (format nil "~a" a))))))))))
